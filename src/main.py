58bb3a2ee0b3a0cccf757ccca1a3987d86221aba
Bryan Anthonio <dev@bryananthonio.com>
2024-12-01 15:33:41 -0800

    more updates

--- src/main.py
+++ src/main.py
@@ -1,41 +1,47 @@
 from fastapi import FastAPI, Request, Response, HTTPException, status
-from redis import Redis
+from contextlib import asynccontextmanager
+
+# from redis import Redis
+from redis.asyncio import Redis
 import os
 from time import time
 import math
 
 # Number of tokens to add to the bucket per second
-BUCKET_REPLENISH_RATE = int(os.getenv("BUCKET_REPLENISH_RATE"))
+BUCKET_REPLENISH_RATE = 2
 
 # Max number of tokens the bucket can carry
-BUCKET_CAPACITY = int(os.getenv("BUCKET_CAPACITY"))
+BUCKET_CAPACITY = 10
 
-TOKENS_PER_REQUEST = int(os.getenv("TOKENS_PER_REQUEST"))
-
-
-def calculate_retry_seconds(current_tokens, tokens_needed, replenish_rate):
-    tokens_to_replenish = tokens_needed - current_tokens
-    if tokens_to_replenish <= 0:
-        return 0
-
-    seconds = math.ceil(tokens_to_replenish / replenish_rate)
-    return seconds
+# Number of tokens needed to make a request
+TOKENS_PER_REQUEST = 1
 
 
-def get_script_hash(redis_client: Redis, rel_path_to_script: str):
+async def get_script_hash(redis_client: Redis, rel_path_to_script: str):
     main_dir = os.path.dirname(os.path.abspath(__file__))
     script_path = os.path.join(main_dir, rel_path_to_script)
 
     with open(script_path, "r") as f:
         script_content = f.read()
 
-    return redis_client.script_load(script_content)
+    return await redis_client.script_load(script_content)
 
 
-redis_client = Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"))
-script_hash = get_script_hash(redis_client, "./request_rate_limiter.lua")
+redis_client = None
+script_hash = None
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global redis_client, script_hash
+    redis_client = Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"))
+    script_hash = await get_script_hash(redis_client, "./request_rate_limiter.lua")
+
+    yield
+    await redis_client.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.middleware("http")
@@ -46,9 +52,11 @@ async def check_request(request: Request, call_next):
     # Redis keys for token bucket
     keys = [prefix + ".tokens", prefix + ".timestamp"]
     args = [BUCKET_REPLENISH_RATE, BUCKET_CAPACITY, time(), TOKENS_PER_REQUEST]
-    allowed, new_tokens = redis_client.evalsha(script_hash, len(keys), *keys, *args)
+    allowed, new_tokens = await redis_client.evalsha(
+        script_hash, len(keys), *keys, *args
+    )
 
-    retry_after = math.ceil(1 / BUCKET_REPLENISH_RATE)
+    retry_after = math.ceil(TOKENS_PER_REQUEST / BUCKET_REPLENISH_RATE)
 
     if allowed:
         response = await call_next(request)
